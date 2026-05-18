ebe9ee81a8f43a7f421cd0f7889098ed9c14f07f
wjaxhson <wjaxhson@gmail.com>
2026-05-18 17:03:22 +0900

    Refactor Firebase initialization and imports
    Refactor Firebase initialization by importing auth and db from shared module, removing direct Firebase config.

--- games/simple-clicker/game.js
+++ games/simple-clicker/game.js
@@ -1,29 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
-  getAuth,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
-  getFirestore,
   doc,
   setDoc,
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDiKBsyStMu6M70dWsW76iattfxj1Ike4k",
-  authDomain: "html5games-cca1b.firebaseapp.com",
-  projectId: "html5games-cca1b",
-  storageBucket: "html5games-cca1b.firebasestorage.app",
-  messagingSenderId: "472070043446",
-  appId: "1:472070043446:web:b57799d5deb6f6bce06597"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+import { auth, db } from "../../shared/firebase.js";
 
 const GAME_ID = "simple-clicker";
 const LOCAL_SAVE_KEY = `html5games:${GAME_ID}:save`;
