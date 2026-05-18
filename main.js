d580d2ab66571177cccee514bdafc585874ec101
wjaxhson <wjaxhson@gmail.com>
2026-05-18 17:02:33 +0900

    Refactor Firebase auth handling in main.js
    Removed Firebase configuration and initialization from main.js. Imported auth from shared/firebase.js instead.

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
-  getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDiKBsyStMu6M70dWsW76iattfxj1Ike4k",
-  authDomain: "html5games-cca1b.firebaseapp.com",
-  projectId: "html5games-cca1b",
-  storageBucket: "html5games-cca1b.firebasestorage.app",
-  messagingSenderId: "472070043446",
-  appId: "1:472070043446:web:b57799d5deb6f6bce06597"
-};
+import { auth } from "./shared/firebase.js";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 const loginBtn = document.getElementById("loginBtn");
